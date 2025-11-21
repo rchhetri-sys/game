a86a339feed2c8fd1767030dcaf88b46adc7e056
RajChhetri <rajchhetri825@gmail.com>
2025-11-20 22:07:36 -0500

    Control info

---
+++ game1/Components/controls.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ControlsInfo = () => {
+  return (
+    <div className="controls-info">
+      <p className="controls-title">Controls:</p>
+      <p>SPACE/↑ - Jump | ← → - Move Left/Right | R - Restart</p>
+    </div>
+  );
+};
+
+export default ControlsInfo;
